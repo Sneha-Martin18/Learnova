fc1a6cf3921ef2eb078b3ead46325ec80fd7e109
jayasurya88 <jayasurya7879@gmail.com>
2025-10-24 20:21:22 +0530

    fix: Add static file serving to URLs

--- student_management_system/urls.py
+++ student_management_system/urls.py
@@ -1,11 +1,13 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.conf import settings
 from django.conf.urls.static import static
-from student_management_system import settings
-
+from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('gateway/', include('gateway.urls')),  # Gateway URLs
-    path('', include('student_management_app.urls')),
-]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path("admin/", admin.site.urls),
+    path("", include("student_management_app.urls")),
+]
+
+# Serve static and media files
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
